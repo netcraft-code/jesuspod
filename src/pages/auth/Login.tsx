07af5a367bce02374e6df52668b28e50e91bd8b3
mdakramraza <mdakramraza916@gmail.com>
2025-12-16 20:16:54 +0530

    add firebasedata in google login time

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -115,6 +115,9 @@ export default function Login() {
                     photoURL: user.photoURL,
                 })
             );
+
+            dispatch(fetchInitialData());
+
             navigate("/home");
         } catch (err: any) {
             alert("Invalid Credential")
